2e59b0a76fae5275a1f5f3bbe7e76770b40bb040
Kéniton Silva <108139767+kenitondev@users.noreply.github.com>
2023-10-07 18:10:09 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Home(){
+    return <h1>Mana, aproveitando esse espaço para 
+        te agradecer por tudo que fez por mim no último ano. 
+        Não teria chegado até aqui sem a sua ajuda, 
+        muito obrigado!</h1>
+}
+
+export default Home;
