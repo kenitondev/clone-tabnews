24be2d6f662b5391be2bef9c86b53b5a9588cf32
Kéniton Silva <108139767+kenitondev@users.noreply.github.com>
2023-11-05 11:51:32 +0000

    instala o `jest` e configura o script test

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <h1>
       Mana, aproveitando esse espaço para te agradecer por tudo que fez por mim
-      no último ano. Não teria chegado até aqui sem a sua ajuda, te amo!
+      no último ano. Não teria chegado até aqui sem a sua ajuda, te amo! s2
     </h1>
   );
 }
